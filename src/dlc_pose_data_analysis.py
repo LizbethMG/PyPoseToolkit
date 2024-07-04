9d0edbe52352765a90cb72b1412ef2fcc1e8c6f4
LizbethMG <lizbeth.mondragon@outlook.fr>
2024-07-04 17:24:20 +0200

    Add methods to get point positions as data frame in experiment.py

--- src/dlc_pose_data_analysis.py
+++ src/dlc_pose_data_analysis.py
@@ -14,31 +14,22 @@ Input: CSV file containing all pose folders with the following structure by colu
 Output Metrics:
     - Averaged Instant Speed: Calculates the animal's average speed over a set period.
     - Activity Level Classification: Categorizes the animal's activity as low, high, or NA (based on a movement threshold) and provides the percentage time spent in each category.
-
+# .\.venv\Scripts\activate
 """
-#Add the src directory to the system path
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 import pandas as pd
+
+#Add the src directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
 from experiment import Experiment
 from get_centroid import *
-
-
+# --------------------------------------
 # Initialize variables and flags
+# --------------------------------------
 isElectrophySyncEnabled = 0  # 1 when sync with electrophy is necessary
-single_experiment = 0 # 0 for all experiments found in the csv file, 1 for specific experiment
-
-# Replace with the path to the CSV file
-csvFilePath = '//l2export/iss02.nerb/nerb-md/decimotiv/Decimotiv_Recording/DREADD_Project/1_PoseAnalysis/Experiments_DLC.csv'
-
-if os.path.exists(csvFilePath):
-    print(f"The file {csvFilePath} exists.")
-else:
-    print(f"The file {csvFilePath} does not exists")
-    exit(1)
-
+single_experiment = 1 # 0 for all experiments found in the csv file, 1 for specific experiment
 # Colors
 yellow = '#EDB120'
 orange = "#D95319"
@@ -46,60 +37,66 @@ purple = "#7E2F8E"
 green = "#77AC30"
 light_blue = "#4DBEEE"
 
-mainDir = os.getcwd()
+# If multiple experiments: replace with the path to the CSV file containing the path to each expeirment to analyze
+csvFilePath = '//l2export/iss02.nerb/nerb-md/decimotiv/Decimotiv_Recording/DREADD_Project/1_PoseAnalysis/Experiments_DLC.csv'
+
+# --------------------------------------
+# Load experiments 
+# --------------------------------------
 print('///////////////////////////////')
-print('Loading the experiments list...')
-
-# Read the CSV file into a DataFrame
-try: 
-    dataTable = pd.read_csv(csvFilePath, sep=';')
-    print(dataTable.head())
-except Exception as e:
-    print(f"Error reading the CSV file: {e}")
-    exit(1)
-
-# Assuming the paths to DLC output are in the 5th column, extract that column, convert the DataFrame column to a list
-Experiments_list = dataTable.iloc[:, 4].tolist()
-
-# Get variable names from the DataFrame if you need them for reference
-variableNames = dataTable.columns.tolist()
-
-print('done!')
-# TODO: If the pose data has to be syncronized with an electrophysiology recording we need to access the TTL. Works for Intan acquired data.
-if isElectrophySyncEnabled == 1:
-    print('Syncronization between video recording and electrophysiology required.')
+print('Loading the list of experiments...')
+if os.path.exists(csvFilePath):
+    print(f">   The file containing the list exists: {csvFilePath}.")
+    try: # Read the CSV file into a DataFrame
+        experimentsTable = pd.read_csv(csvFilePath, sep=';')
+        path_list = experimentsTable.iloc[:,4]
+        print(f">   It lists {path_list.size} experiments.")
+    except Exception as e:
+        print(f"Error reading the CSV file: {e}")
+        exit(1)
 else:
-    print('No video syncronization with electrophysiology required.')
+        print(f"The file {csvFilePath} does not exists")
+        exit(1)
+print('Done!')  
 
-'''From Deeplabcut files (camera 1 and camera 2 facing each other) the function extracts bodyparts, 
-creates a polygom approximating the body contour ans calculates the centroid. '''
-
-# For single experiment analysis, select the experiment to analyse
+# For single experiment analysis or manual selection , select the experiment to analyse
 if single_experiment == 1:
+    print('Single experiment selected:')
     single_experiment = Experiment(
         animal = 755,
         compound = "21",
         dose = "1", # mg/kg
         timepoint = 0, # hours
-        dataTable= dataTable
+        experimentsTable=experimentsTable,
     ) 
-    path_list = single_experiment.get_experiment_path()
-    print(f"Experiment path: {path_list}")
-elif single_experiment == 0:
-    path_list = dataTable.iloc[:,4]
-    print(f"The file contains {path_list.size} files to analyze.")
+    
+# Multiple experiments to analyze listed in a csv file 
+elif single_experiment == 0: 
+    
+        # Assuming the paths to DLC output are in the 5th column, extract that column, convert the DataFrame column to a list
+        Experiments_list = experimentsTable.iloc[:, 4].tolist()
+        # Get variable names from the DataFrame if you need them for reference
+        variableNames = experimentsTable.columns.tolist()
+        for i in path_list.index:
+            print(f">   Starting to analyze experiment {i+1} / {path_list.size}: ")
+            print(f">   Animal ID {experimentsTable.iloc[i,0]}, compound {experimentsTable.iloc[i,1]}, dose {experimentsTable.iloc[i,2]} mg/kg, timepoint {experimentsTable.iloc[i,3]} h post injection")
+
+            path_to_experiment = path_list[i]
+            
+            #TODO: For the moment this scripts parts from the fact that centroid calculation was done elsewhere
+            # and saved in a csv file 
+            #Cc, Sry, Cc_array = get_centroid(path_to_experiment)
+            
+            if os.path.exists(path_to_experiment):
+                
+                print(f"The file {path_to_experiment} exists.")
+                point_positions = get_positions(path_to_experiment)
+            else:
+                print(f"The file {path_to_experiment} does not exists")
+                exit(1)
+
+    
 else: 
     print(f"Error selecting experiment to anaylse")
-
-for i in path_list.index:
-    print(f">   Starting to analyze experiment {i+1} / {path_list.size}: ")
-    print(f">   Animal ID {dataTable.iloc[i,0]}, compound {dataTable.iloc[i,1]}, dose {dataTable.iloc[i,2]} mg/kg, timepoint {dataTable.iloc[i,3]} h post injection")
-
-    path_to_experiment = path_list[i]
-
-    #TODO: For the moment this scripts parts from the fact that centroid calculation was done elsewhere
-    #Cc, Sry, Cc_array = get_centroid(path_to_experiment)
-    
-
-
+print('done!')
 print(r'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
--- src/experiment.py
+++ src/experiment.py
@@ -1,10 +1,18 @@
+import glob2
+import os
+import pandas as pd
+
 class Experiment: 
-    def __init__(self, animal, compound, dose, timepoint, dataTable):
+    path_to_experiment = ''
+    point_positions = None
+    def __init__(self, animal, compound, dose, timepoint, experimentsTable):
         self.animal = animal # id number ex. 755
         self.compound = compound # compound name
         self.dose = dose # mg/kg
         self.timepoint = timepoint # hours after injection
-        self.dataTable = dataTable
+        self.experimentsTable = experimentsTable
+        self.get_experiment_path()
+        self.get_point_positions()
 
     def get_experiment_path(self):
         """
@@ -12,17 +20,39 @@ class Experiment:
         Returns:
         The value in the fifth column if a match is found, otherwise None.
         """
-        result = self.dataTable[
-            (self.dataTable.iloc[:,0] == self.animal) &
-            (self.dataTable.iloc[:,1] == self.compound) &
-            (self.dataTable.iloc[:,2] == self.dose) &
-            (self.dataTable.iloc[:,3] == self.timepoint)
+        result = self.experimentsTable[
+            (self.experimentsTable.iloc[:,0] == self.animal) &
+            (self.experimentsTable.iloc[:,1] == self.compound) &
+            (self.experimentsTable.iloc[:,2] == self.dose) &
+            (self.experimentsTable.iloc[:,3] == self.timepoint)
         ]
         if not result.empty:
-            return result.iloc[0,4]
-        else: 
-            return None
+            print(f">   Experiment path: {result.iloc[0,4]}")
+            self.path_to_experiment = result.iloc[0,4]
 
-
-    # Debug print to confirm method existence
-    print("Experiment class loaded with get_experiment_path method.")
+    def get_point_positions(self):
+        """
+        Finds the first Excel file in the specified folder and reads a specific sheet.
+        Inputs:
+        - folder_path: Path to the folder containing the Excel file.        
+        Outputs:
+        - DataFrame containing the data from the specified sheet, or None if no Excel file is found or an error occurs.
+        """
+        # Use glob to find all .xlsx files in the folder
+        excel_file = glob2.glob(os.path.join(self.path_to_experiment, '*.xlsx'))
+    
+        if excel_file:
+            # If there are any .xlsx files, take the first one
+            file_path = excel_file[0]
+            print(f">   Found Excel file containg the data points for experiment:")
+            print(f">   Animal ID {self.animal}, compound {self.compound}, dose {self.dose} mg/kg, timepoint {self.timepoint} h post injection")
+            try:
+                # Read the specified sheet from the Excel file
+                self.point_positions = pd.read_excel(file_path, sheet_name='Coord_centroid')
+            except Exception as e:
+                print(f"An error occurred while reading the Excel file: {e}")
+                
+        else:
+            print("No Excel files found in the specified folder.")
+           
+        
